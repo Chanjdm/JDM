a1d70fe3aa53817861b4c13b1d06bbac69c57e41
Chanjdm <99297859+Chanjdm@users.noreply.github.com>
2023-08-04 15:39:24 -0600

    Update jdmpelis.js

--- JDM/jdmpelis.js
+++ JDM/jdmpelis.js
@@ -46,3 +46,88 @@
         }
       }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        var codigoCorrecto = "51078"; // Cambia "miCodigoSecreto" por tu código de acceso
+        var tiempoDuracion = 24 * 60 * 60 * 1000; // 24 horas en milisegundos
+
+        function verificarCodigo() {
+            var codigoIngresado = document.getElementById("clave").value;
+
+            if (codigoIngresado === codigoCorrecto) {
+                localStorage.setItem("accesoConcedido", "true");
+                mostrarContenido();
+            } else {
+                alert("Código incorrecto. Intenta de nuevo.");
+                document.getElementById("clave").value = "";
+            }
+        }
+
+        function mostrarContenido() {
+            document.getElementById("formulario").style.display = "none";
+            document.getElementById("contenido").style.display = "block";
+            setTimeout(cerrarSesion, tiempoDuracion); // Configurar el temporizador para cerrar la sesión
+        }
+
+        function cerrarSesion() {
+            localStorage.removeItem("accesoConcedido");
+            document.getElementById("contenido").style.display = "none";
+            document.getElementById("formulario").style.display = "block";
+            document.getElementById("clave").value = "";
+        }
+
+        // Comprobar si la sesión está activa al cargar la página
+        window.onload = function() {
+            var accesoConcedido = localStorage.getItem("accesoConcedido");
+            if (accesoConcedido === "true") {
+                mostrarContenido();
+            } else {
+                cerrarSesion();
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // Deshabilitar la apertura de ventanas emergentes y enlaces externos
+  window.addEventListener("beforeunload", function (event) {
+    // Detener la apertura de ventanas emergentes
+    window.open = function() { return null; };
+
+    // Desactivar todos los enlaces que apunten a páginas externas
+    var externalLinks = document.querySelectorAll("a[target='_blank']");
+    for (var i = 0; i < externalLinks.length; i++) {
+      externalLinks[i].removeAttribute("target");
+    }
+  });
